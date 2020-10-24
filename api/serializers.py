b4abb6f13e0afae82c2fdd207043284ed8b66ec6
vivekascoder <vivekascoder@gmail.com>
2020-10-24 20:02:35 +0000

    Day Two Code

--- api/serializers.py
+++ api/serializers.py
@@ -3,18 +3,24 @@ import datetime
 from api.models import Todo
 
 
-class TodoSerializer(serializers.Serializer):
-    task = serializers.CharField(required=True)
-    is_completed = serializers.BooleanField(default=False)
-    time = serializers.DateTimeField(default=datetime.datetime.now())
+# class TodoSerializer(serializers.Serializer):
+#     task = serializers.CharField()
+#     is_completed = serializers.BooleanField(default=False)
+#     time = serializers.DateTimeField(default=datetime.datetime.now())
 
-    def create(self, validated_data):
-        return Todo.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return Todo.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        instance.task = validated_data.get('task', instance.task)
-        instance.is_completed = validated_data.get('is_completed', instance.is_completed)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         print(validated_data)
+#         instance.task = validated_data.get('task', instance.task)
+#         instance.is_completed = validated_data.get('is_completed', instance.is_completed)
+#         instance.save()
+#         return instance
     
 
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['task', 'is_completed', 'time']
+        # fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -3,15 +3,18 @@ from api.models import Todo
 from django.http import JsonResponse
 from api.serializers import TodoSerializer
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
-
+@api_view(['GET', 'PUT', 'DELETE'])
 def todo(request, pk):
     def get_queryset():
         try:
             todo = Todo.objects.get(pk=pk)
         except Todo.DoesNotExist:
-            return JsonResponse(
+            return Response(
                 {
                     "error": f"There is no todo having primary key {pk}"
                 }
@@ -20,20 +23,39 @@ def todo(request, pk):
     
     if request.method == "GET":
         todo = get_queryset()
-        serializer = TodoSerializer(instance=todo)
-        return JsonResponse(serializer.data, safe=False)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-@csrf_exempt
-def create_todo(request):
-    if request.method == "GET":
-        return JsonResponse({'error': "GET is not allowed"})
-    elif request.method == "POST":
-        serializer = TodoSerializer(data=request.POST)
+    elif request.method == "PUT":
+        todo = get_queryset()
+        print(request.data)
+        serializer = TodoSerializer(instance=todo, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=False)
+            return Response(serializer.data)
         else:
-            return JsonResponse(serializer.errors, safe=False)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == "DELETE":
+        todo = get_queryset()
+        todo.delete()
+        return Response({'msg': "Deleted"})
+
+
+@api_view(['GET', 'POST'])
+def create_todo(request):
+    if request.method == "GET":
+        queryset = Todo.objects.all()
+        serializer = TodoSerializer(instance=queryset, many=True)
+        return Response(data=serializer.data)
+
+    elif request.method == "POST":
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 
 
